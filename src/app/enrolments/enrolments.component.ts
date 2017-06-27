96d6c313f1a36c966b3654febf13324dde16343d
michal <michal.labuda@edps.com.pl>
2017-06-27 22:05:56 +0200

    Merge

--- src/app/enrolments/enrolments.component.ts
+++ src/app/enrolments/enrolments.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Enrolment } from '../models/enrolment'
 import { EnrolmentService } from '../services/enrolment.service';
@@ -20,73 +19,30 @@ export class EnrolmentsComponent implements OnInit, OnChanges {
     @Input() editId: string;
 
     // Constructor with injected service
-    constructor(private enrolmentService: EnrolmentService) {}
+    constructor( private enrolmentService: EnrolmentService ) { }
 
     ngOnInit() {
-            // Load comments
-            this.loadEnrolments()
+        // Load comments
+        this.loadEnrolments()
     }
 
     loadEnrolments() {
         // Get all enrolments
-         this.enrolmentService.getEnrolments()
-                           .subscribe(
-                                   enrolments => this.enrolments = enrolments, //Bind to view
-                                err => {
-                                    // Log errors if any
-                                    console.log(err);
-                                });
-         
-         console.log(this.enrolments);
+        this.enrolmentService.getEnrolments()
+            .subscribe(
+            enrolments => this.enrolments = enrolments, //Bind to view
+            err => {
+                // Log errors if any
+                console.log( err );
+            } );
+
+        console.log( this.enrolments );
     }
 
-    ngOnChanges(changes:any) {
+    ngOnChanges( changes: any ) {
         // Listen to the 'list'emitted event so as populate the model
         // with the event payload
-        EmitterService.get(this.listId).subscribe((enrolments:Enrolment[]) => { this.loadEnrolments()});
-=======
-import { Component, OnInit, Input } from '@angular/core';
-import { Enrolment } from '../models/enrolment'
-import { EnrolmentService } from '../services/enrolment.service';
-import { EmitterService } from '../services/emitter.service';
+        EmitterService.get( this.listId ).subscribe(( enrolments: Enrolment[] ) => { this.loadEnrolments() } );
 
-@Component( {
-    selector: 'app-enrolments',
-    templateUrl: './enrolments.component.html',
-    styleUrls: ['./enrolments.component.css']
-} )
-export class EnrolmentsComponent implements OnInit {
-
-    constructor(
-        private enrolmentService: EnrolmentService
-    ) { }
-
-    ngOnInit() {
-    }
-
- // Define input properties
-    @Input() enrolment: Enrolment;
-    @Input() listId: string;
-    @Input() editId:string;
-    
-    response;
-    helloWorldRespone : string;
-    getHelloResponse() {
-        this.response = this.enrolmentService.getHello();
-    }
-    
-    
-    getEnrolments(id:string) {
-        // Call removeComment() from CommentService to delete comment
-        this.enrolmentService.getEnrolments().subscribe(
-                                enrolments => {
-                                    // Emit list event
-                                    EmitterService.get(this.listId).emit(enrolments);
-                                }, 
-                                err => {
-                                    // Log errors if any
-                                    console.log(err);
-                                });
->>>>>>> refs/remotes/origin/master
     }
 }
