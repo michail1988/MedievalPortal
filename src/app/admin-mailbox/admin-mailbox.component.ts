f216f390dac626d2cd5396b59313a35f445b8e3e
michal <michal.labuda@edps.com.pl>
2017-11-14 18:10:17 +0100

    Merge

--- src/app/admin-mailbox/admin-mailbox.component.ts
+++ src/app/admin-mailbox/admin-mailbox.component.ts
@@ -30,7 +30,6 @@ export class AdminMailboxComponent implements OnInit {
 
     users: User[];
 
-<<<<<<< HEAD
     addressees: SelectItem[];
 
     showAllEnabled: boolean;
@@ -171,122 +170,6 @@ export class AdminMailboxComponent implements OnInit {
         this.showAcceptedEnabled = false;
         this.showNotAcceptedEnabled = false;
         this.showSpeakersEnabled = false;
-=======
-    addressees: SelectItem[];
 
-    constructor( private contactService: ContactService, private userService: UserService ) {
-
-
-    }
-
-    ngOnInit() {
-        this.showAll();
-    }
-
-    submitMessage() {
-
-                this.contactMessage.date = new Date();
-                this.contactMessage.email = ''
-                    
-                for ( var i = 0; i < this.selectedAddresses.length; i++ ) {
-                    
-                    if (this.isEmpty(this.contactMessage.email)) {
-                        this.contactMessage.email = this.selectedAddresses[i].code;
-                    } else {
-                        this.contactMessage.email = this.contactMessage.email + ', ' + this.selectedAddresses[i].code;
-                    }
-                }
-                
-                console.log( 'Wysylam do ' +  this.contactMessage.email);
-                this.contactService.sendAdminMessages( this.contactMessage ).subscribe(
-                    response => {
-                        this.msgs = [];
-                        this.msgs.push( { severity: 'success', summary: 'Email dostarczony.', detail: '' } );
-        
-                        this.submitted = true;
-                    },
-                    err => {
-                        // Log errors if any
-                        console.log( err );
-                    } );
-        
-        
-    }
-
-    showAll() {
-        this.userService.getUsers()
-            .subscribe(
-            allUsers => {
-                this.users = allUsers;
-                this.setItems();
-            },
-            err => {
-                console.log( err );
-            } );
-
-        this.setItems();
-    }
-
-    showAcepted() {
-        this.userService.getAcceptedUsers()
-            .subscribe(
-            acceptedUsers => {
-                this.users = acceptedUsers;
-                this.setItems();
-            },
-            err => {
-                console.log( err );
-            } );
-
-        this.setItems();
-    }
-
-    showNotAccepted() {
-        this.userService.getNotAcceptedUsers()
-            .subscribe(
-            notAcceptedUsers => {
-                this.users = notAcceptedUsers;
-                this.setItems();
-            },
-            err => {
-                console.log( err );
-            } );
-
-        this.setItems();
-    }
-
-    showSpeakers() {
-        this.userService.getSpeakers()
-            .subscribe(
-            speakers => {
-                this.users = speakers;
-                this.setItems();
-            },
-            err => {
-                console.log( err );
-            } );
-
-        this.setItems();
-    }
-
-    setItems() {
-        this.addressees = [];
-        this.selectedAddresses = [];
-
-        if ( this.users ) {
-            for ( var i = 0; i < this.users.length; i++ ) {
-
-                let name = this.users[i].name + ' ' + this.users[i].surname;
-                this.addressees.push( { label: name, value: { id: i + 1, name: name, code: this.users[i].email } } );
-
-            }
-        }
-
-        this.addressees.sort();
-    }
-    
-    isEmpty(str) {
-        return (!str || 0 === str.length);
->>>>>>> branch 'master' of https://github.com/michail1988/MedievalPortal.git
     }
 }
