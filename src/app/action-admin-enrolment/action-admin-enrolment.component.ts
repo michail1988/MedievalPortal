cedce144c8d3e978fcc4ad2d140b7c6886310f42
michal <michal.labuda@edps.com.pl>
2018-03-28 00:14:34 +0200

    Confirmation dialog dla odrzucania uczestnika

--- src/app/action-admin-enrolment/action-admin-enrolment.component.ts
+++ src/app/action-admin-enrolment/action-admin-enrolment.component.ts
@@ -1,69 +1,87 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from "app/models/user";
 import { UserService } from "app/services/user.service";
+import { ConfirmationService } from "primeng/primeng";
+import { Message } from "primeng/components/common/api";
 
 @Component( {
     selector: 'action-admin-enrolment',
     templateUrl: './action-admin-enrolment.component.html',
-    styleUrls: ['./action-admin-enrolment.component.css']
+    styleUrls: ['./action-admin-enrolment.component.css'],
+    providers: [ConfirmationService]
 } )
 export class ActionAdminEnrolmentComponent implements OnInit {
 
-    public renderValue;
+    msgs: Message[] = [];
 
-    @Input() value : User;
+    @Input() value: User;
 
-    constructor(private userService: UserService) { }
+    constructor( private userService: UserService, private confirmationService: ConfirmationService ) { }
 
     ngOnInit() {
-        this.renderValue = this.value;
     }
 
     example() {
         alert( this.value.name );
     }
-    
+
     isAcceptationPending() {
-        if ('Y' == this.value.confirmation || 'N' == this.value.confirmation) {
+        if ( 'Y' == this.value.confirmation || 'N' == this.value.confirmation ) {
             return false;
         }
-        
+
         return true;
     }
     accept() {
         //TODO refresh lub dymek
         this.userService.acceptUser( this.value ).subscribe(
-                response => {
+            response => {
 
 
-                    if ( response.text() === 'OK' ) {
-                        -                    console.log( 'udalo sie accepted '  + response.text());
-                        //           
-                        this.value.confirmation = 'Y'
-                    }
-                },
-                err => {
-                    // Log errors if any
-                    console.log( err );
-                } );
+                if ( response.text() === 'OK' ) {
+                    -                    console.log( 'udalo sie accepted ' + response.text() );
+                    //           
+                    this.value.confirmation = 'Y'
+                }
+            },
+            err => {
+                // Log errors if any
+                console.log( err );
+            } );
     }
-    
+
+    confirmReject() {
+        this.confirmationService.confirm( {
+            message: 'Czy na pewno chcesz odrzucic zgloszenie?',
+            header: 'Potwierdz odrzucenie',
+            icon: 'fa fa-trash',
+            accept: () => {
+                this.msgs = [{ severity: 'info', summary: 'Confirmed', detail: 'Record deleted' }];
+
+                this.reject();
+            },
+            reject: () => {
+                this.msgs = [{ severity: 'info', summary: 'Rejected', detail: 'You have rejected' }];
+            }
+        } );
+    }
+
     reject() {
-      //TODO refresh lub dymek
+        //TODO refresh lub dymek
         this.userService.rejectUser( this.value ).subscribe(
-                response => {
+            response => {
 
 
-                    if ( response.text() === 'OK' ) {
-                        -                    console.log( 'udalo sie rejected '  + response.text());
-                        
-                        this.value.confirmation = 'N'
-                        //           
-                    }
-                },
-                err => {
-                    // Log errors if any
-                    console.log( err );
-                } );
+                if ( response.text() === 'OK' ) {
+                    -                    console.log( 'udalo sie rejected ' + response.text() );
+
+                    this.value.confirmation = 'N'
+                    //           
+                }
+            },
+            err => {
+                // Log errors if any
+                console.log( err );
+            } );
     }
 }
