476d559eef7664ae2ed553818e465715ea6906a6
michal <michal.labuda@edps.com.pl>
2018-04-04 20:20:42 +0200

    Fix: Error encountered resolving symbol values statically

--- src/app/translations/translations.ts
+++ src/app/translations/translations.ts
@@ -8,11 +8,15 @@ import { LANG_PL_NAME, LANG_PL_TRANS } from './lang-pl'
 //translation token
 export const TRANSLATIONS = new OpaqueToken( 'translations' );
 
+// Fix: Error encountered resolving symbol values statically
+//https://github.com/angular/angular/issues/13983
+//
+
 //all translations
 export const dictionary = {
-        LANG_EN_NAME: LANG_EN_TRANS,
-        LANG_DE_NAME: LANG_DE_TRANS,
-        LANG_PL_NAME: LANG_PL_TRANS,
+        'en': LANG_EN_TRANS,
+        'de': LANG_DE_TRANS,
+        'pl': LANG_PL_TRANS,
 }
 
 //providers
